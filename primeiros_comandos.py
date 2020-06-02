966b7a09d5ec0e8b1fbc0fbc8fdd6a2d65ac6381
luryrodrigues <lurylrodrigues@gmail.com>
2020-06-02 19:53:57 -0300

    aula 6

---
+++ primeiros_comandos.py
@@ -0,0 +1,15 @@
+print('Hello World!')
+
+print('Quanto é 2 + 3?')
+a=2
+b=3
+soma=a+b
+print(soma)
+
+print('Quanto é 2 e 3?')
+print('2'+'3')
+
+nome='Lury'
+idade=25
+peso=48
+print(nome,idade,peso)
---
+++ tipos_primitivos_e_saida_de_dados.py
@@ -0,0 +1,13 @@
+nome=input('Qual é o seu nome? ')
+idade=input('Qual a sua idade? ')
+peso=input('Quanto você pesa? ')
+print(nome,idade,peso)
+
+nome=input('Qual seu nome? ')
+print('Olá,',nome,'! Seja bem vindo(a)!')
+print('Olá, {}! Seja bem-vindo(a)!'.format(nome))
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1 + n2
+print('A soma é igual a : ',s)
