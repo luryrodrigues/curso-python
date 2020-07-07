65d5266d099ad198cd44c8601d5d88dd7707c190
luryrodrigues <lurylrodrigues@gmail.com>
2020-07-07 11:48:01 -0300

    renomeando_arquivos

--- dissecando_variaveis.py
+++
@@ -1,9 +0,0 @@
-# função is retorna informações sobre a variável
-x=input('Digite algo: ')
-print('O tipo primitivo desse valor é: ',type(x))
-print('Só tem espaços? ',x.isspace())
-print('É um número? ',x.isnumeric())
-print('É alfabético? ',x.isalpha())
-print('É alfanumérico? ',x.isalnum())
-print('Está em letras minúsculas? ',x.islower())
-print('Está capitalizada? ',x.istitle())
